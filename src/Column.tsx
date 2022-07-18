f33f5a0d70c6281c92020a904bc7b31b89e7124a
KaylaKZJ <kaylajoubert33@gmail.com>
2022-07-18 16:33:13 +0200

    Adds card and add new item functionality

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,16 +1,23 @@
+import { Card } from "./Card";
 import { ColumnContainer, ColumnTitle, CardContainer } from "./styles";
+import { AddNewItem } from "./AddNewItem";
 
 type ColumnProps = {
   text: string;
+  children?: React.ReactNode;
 };
 
 export const Column = ({ text }: ColumnProps) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      <CardContainer>Generate app scaffold</CardContainer>
-      <CardContainer>Learn TypeScript</CardContainer>
-      <CardContainer>Begin to use static typing</CardContainer>
+      <Card text="Generate app scaffold" />
+      <Card text="Learn TypeScript" />
+      <Card
+        text="Begin to use static typing
+      "
+      />
+      <AddNewItem toggleButtonText="+ Add another card" onAdd={console.log} />
     </ColumnContainer>
   );
 };
